01dc252a636b8ec08422d667b786607c4c5f2c5b
Gus Hahn-Powell <gushahnpowell@gmail.com>
2021-08-31 13:13:08 -0700

    Update build-docker-image.sh

--- {{cookiecutter.repo_name}}/build-docker-image.sh
+++ {{cookiecutter.repo_name}}/build-docker-image.sh
@@ -1,4 +1,4 @@
 DOCKERFILE="Dockerfile"
-ORG="parsertongue"
+ORG="{{cookiecutter.organization}}"
 IMAGE_NAME="{{cookiecutter.package_image_name}}"
 docker buildx build --output=type=docker --platform linux/amd64 -f ${DOCKERFILE} -t "${ORG}/${IMAGE_NAME}:latest" -t "${ORG}/${IMAGE_NAME}:amd64" .
