44ee38518ba21577358c4d7eb97e4b470ea80e30
Gus Hahn-Powell <gushahnpowell@gmail.com>
2021-09-01 22:55:53 -0700

    Include link to docs fo unittest.TestCase

--- {{cookiecutter.repo_name}}/tests/test_example.py
+++ {{cookiecutter.repo_name}}/tests/test_example.py
@@ -3,6 +3,7 @@ from {{cookiecutter.namespace}}.{{cookiecutter.package_name}} import info
 from .utils import EXAMPLE
 import unittest
 
+# see https://docs.python.org/3/library/unittest.html#basic-example
 class TestTemplate(unittest.TestCase):
 
     def test_example(self):
