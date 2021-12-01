0a3f37b26df8d4f5cb236c56c11ec3455f3c2734
Gus Hahn-Powell <gushahnpowell@gmail.com>
2021-12-01 01:45:02 -0700

    Removed ExtractionResult

--- templates/odin/{{cookiecutter.repo_name}}/reader/src/main/scala/{{cookiecutter.class_path}}/mr/ExtractionResult.scala
+++
@@ -1,9 +0,0 @@
-package {{ cookiecutter.class_path }}.mr
-
-import org.clulab.odin.Mention
-
-/** Mentions associated with some document metadata */
-case class ExtractionResult(
-  metadata: DocumentMetadata,
-  mention: Mention
-)
