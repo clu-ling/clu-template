39eb632dd791b2c0e557e22047832aa08c1395cd
zwellington <zdwellington@email.arizona.edu>
2021-12-01 09:20:06 -0700

    path variable

--- templates/odin/hooks/pre-gen-project.py
+++ templates/odin/hooks/pre-gen-project.py
@@ -1,6 +1,8 @@
 import sys
 import requests
 
+{% set class_dir = cookiecutter.class_path.replace('.','/') %}
+
 repo_org = "{{ cookiecutter.repo_organization }}"
 git_link = f"http://github.com/{repo_org}"
 response = requests.get(git_link)
